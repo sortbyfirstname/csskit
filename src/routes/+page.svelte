398b446b7a03605380217261de32bfd15f966458
Aaron Seabrook <mail@aaronseabrook.com>
2023-04-19 16:43:21 +0100

    init propper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,6 @@
 </div>
 
 <div class="flex flex-row space-x-8">
-	<div class="relative">
-		<div class="absolute -rotate-12 -left-3 top-3 bg-teal-400 text-white font-mono whitespace-nowrap px-3">
-			COMING SOON
-		</div>
-		<button class="btn btn-primary" on:click={() => goto('/propper')} disabled>Propper</button>
-	</div>
+	<button class="btn btn-primary" on:click={() => goto('/propper')}>Propper</button>
 	<button class="btn btn-primary" on:click={() => goto('/rainbow')}>Rainbow</button>
 </div>
