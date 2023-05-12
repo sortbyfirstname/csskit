33e077803808f57b299f4aece080e9bfd5edfa43
Aaron Seabrook <mail@aaronseabrook.com>
2023-05-12 02:49:39 +0100

    rerun prettier on all files

--- src/lib/components/Animation.svelte
+++ src/lib/components/Animation.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let src: string;
 	export let loop: boolean = false;
-    export let autoplay: boolean = false;
+	export let autoplay: boolean = false;
 	export let hover: boolean = false;
 </script>
 
@@ -9,4 +9,4 @@
 	<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 </svelte:head>
 
-<lottie-player {src} class={$$props.class} background="transparent" speed="1" {loop} {autoplay} {hover}  />
+<lottie-player {src} class={$$props.class} background="transparent" speed="1" {loop} {autoplay} {hover} />
--- src/routes/api/cssnano/+server.ts
+++ src/routes/api/cssnano/+server.ts
@@ -1,8 +1,8 @@
 import { nano } from '$lib/utils/nanonow.js';
 import { json } from '@sveltejs/kit';
 
-export async function POST({request}) {
-    const css: string = await request.json();
-    const min = await nano(css);
-    return json(min);
+export async function POST({ request }) {
+	const css: string = await request.json();
+	const min = await nano(css);
+	return json(min);
 }
